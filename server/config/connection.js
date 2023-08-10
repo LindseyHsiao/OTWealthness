376fd0d6881b09a01ff9f2cd697393a129d69786
Lindsey Sherrill <lindsey350@aol.com>
2023-08-10 11:43:25 -0400

    updated

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-mongoose.connect(process.env.MONGODB_URI || process.env.ATLAS)
+mongoose.connect(process.env.ATLAS)
 
 module.exports =  mongoose.connection;
